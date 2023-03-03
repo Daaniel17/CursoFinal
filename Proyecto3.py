24d2095e69505763c6d0e75ef2cb0c9be8cbdc93
Daaniel17 <danielsan1798@hotmail.com>
2023-03-02 21:15:23 -0500

    Avance

---
+++ Proyecto3.py
@@ -0,0 +1,12 @@
+texto=input("Ingrese el texto: ")
+texto=texto.lower()
+#letras=input("Ingrese 3 letras de su eleccion: ")
+#letras=letras.lower()
+
+ 
+
+cantidad=texto.split()
+cantidad=len(cantidad)
+print("La cantidad de palabras que hay en este texto son: ",cantidad)
+
+print("La primera letra del texto es: ",texto[0])
